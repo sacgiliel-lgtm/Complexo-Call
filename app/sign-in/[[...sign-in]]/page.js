0068ef9dfc2ecebdc287ff22f4588b508c7e17f9
sacgiliel-lgtm <sacgiliel@gmail.com>
2026-09-20 13:53:45 -0300

    fix(auth): redirect legacy sign-in route to custom CPX auth

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,11 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-  return (
-    <main className="login-page" style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', padding: 24 }}>
-      <SignIn fallbackRedirectUrl="/servidor" />
-    </main>
-  );
+export default function AuthRedirect() {
+  redirect('/');
 }
